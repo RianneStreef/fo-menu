b7d9af80db97a75294fa5c3e2901c92c5516ad46
RianneStreef <riannestreef@gmail.com>
2021-11-10 12:33:17 +0100

    dissapearing menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,35 +7,34 @@ import homepage from "../images/icon.png";
 
 const Nav = () => {
   useEffect(() => {
-    const body = document.body;
+    const nav = document.getElementById("nav");
+    console.log(nav);
     const scrollUp = "scroll-up";
     const scrollDown = "scroll-down";
     let lastScroll = 0;
     window.addEventListener("scroll", () => {
       const currentScroll = window.pageYOffset;
       if (currentScroll <= 0) {
-        body.classList.remove(scrollUp);
+        nav.classList.remove(scrollUp);
         return;
       }
 
-      if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-        // down
-        body.classList.remove(scrollUp);
-        body.classList.add(scrollDown);
+      if (currentScroll > lastScroll && !nav.classList.contains(scrollDown)) {
+        nav.classList.remove(scrollUp);
+        nav.classList.add(scrollDown);
       } else if (
         currentScroll < lastScroll &&
-        body.classList.contains(scrollDown)
+        nav.classList.contains(scrollDown)
       ) {
-        // up
-        body.classList.remove(scrollDown);
-        body.classList.add(scrollUp);
+        nav.classList.remove(scrollDown);
+        nav.classList.add(scrollUp);
       }
       lastScroll = currentScroll;
     });
   });
 
   return (
-    <nav className="nav" id="nav">
+    <nav className="nav scroll-up" id="nav">
       <a href="javascript:history.back()" className="back-icon">
         <img src={back} />
       </a>
